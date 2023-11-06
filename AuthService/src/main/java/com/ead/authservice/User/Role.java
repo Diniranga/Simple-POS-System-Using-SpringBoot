15bb018f25145662a13bdda192beadbd2a41b9bb
diniranga <dinirangapremanayake@gmail.com>
2023-11-06 22:10:12 +0530

    Initial commit

---
+++ AuthService/src/main/java/com/ead/authservice/User/Role.java
@@ -0,0 +1,7 @@
+package com.ead.authservice.User;
+
+public enum Role {
+    CUSTOMER,
+    INVENTORY_KEEPER,
+    DELIVERY_PERSON,
+}
---
+++ DeliveryService/src/main/java/com/ead/deliveryservice/Controller/DeliveryPersonControler.java
@@ -0,0 +1,36 @@
+package com.ead.deliveryservice.Controller;
+
+import com.ead.deliveryservice.Entity.DeliveryPerson;
+import com.ead.deliveryservice.Service.DeliveryPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/deliveryPerson")
+public class DeliveryPersonControler {
+
+    @Autowired
+    private DeliveryPersonService deliveryPersonService;
+
+    @GetMapping("/getAllDeliveryPerson")
+    public ResponseEntity<?> getAllDeliveryPerson() {
+        return deliveryPersonService.getAllDeliveryPerson();
+    }
+
+    @GetMapping("/getDeliveryPersonById/{deliveryPersonId}")
+    public DeliveryPerson getDeliveryPersonById(@PathVariable String deliveryPersonId){
+        return deliveryPersonService.getDeliverPersonById(deliveryPersonId);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> saveDeliveryPerson(@RequestBody DeliveryPerson deliveryPerson) {
+        return deliveryPersonService.saveDeliveryPerson(deliveryPerson);
+    }
+
+    @PutMapping("/update/{deliveryPersonId}")
+    public ResponseEntity<?> updateDeliveryPerson(@PathVariable String deliveryPersonId,@RequestBody DeliveryPerson updatedDeliveryPerson) {
+        return deliveryPersonService.updateDeliveryPerson(deliveryPersonId,updatedDeliveryPerson);
+    }
+
+}
